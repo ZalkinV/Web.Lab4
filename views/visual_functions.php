ffcddc897a7186523161fdb4648dd5dea1334641
Zalkin <Zalkin.viktor@mail.ru>
2019-05-02 17:49:37 +0300

    V.VF. Implement shorten_text()

--- views/visual_functions.php
+++ views/visual_functions.php
@@ -1,1 +1,15 @@
 <?php
+
+function shorten_text($text, $words_count = 40)
+{
+    $all_words = preg_split("/[\s]+/", $text);
+    $words = array_slice($all_words, 0, $words_count);
+    $short_text = join(" ", $words);
+
+    if (count($all_words) > $words_count)
+        $short_text.="...";
+
+    return $short_text;
+}
+
+?>
