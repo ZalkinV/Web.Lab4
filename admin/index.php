ee9dc8f4f80b40779885c04ec24226eb03fb9baa
Zalkin <Zalkin.viktor@mail.ru>
2019-05-02 20:44:03 +0300

    A.I. Add getting GET parameter - action

--- admin/index.php
+++ admin/index.php
@@ -2,6 +2,11 @@
 require_once "../models/database.php";
 require_once "../views/header.html";
 require_once "../views/visual_functions.php";
+
+
+if (isset($_GET["action"]))
+    $action = $_GET["action"];
+
 ?>
 
 <div class="container">
