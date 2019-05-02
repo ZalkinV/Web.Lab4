906c8b892dc944323a53b64701e725ce968755da
Zalkin <Zalkin.viktor@mail.ru>
2019-05-02 16:51:11 +0300

    I. Add div with articles

--- index.php
+++ index.php
@@ -2,3 +2,19 @@
 require_once "models/database.php";
 require_once "views/header.html";
 ?>
+
+<div class="container">
+    <?php
+    $articles = get_articles();
+    foreach ($articles as $article): ?>
+    <h1>
+        <a href="#">
+            <?= $article["title"] ?>
+        </a>
+    </h1>
+    <p>
+        <?= $article["text"] ?>
+    </p>
+    <hr>
+    <?php endforeach; ?>
+</div>
