ed59337780bf0a202df994f3da37bc1642b36cf1
Zalkin <Zalkin.viktor@mail.ru>
2019-05-02 21:23:44 +0300

    A.AA. Add form for adding article

--- admin/article_adding.php
+++ admin/article_adding.php
@@ -1,1 +1,18 @@
 <?php
+require_once "../views/header.html";
+?>
+
+<div class="container">
+    <h1>Create new article</h1>
+    <form method="post" action="index.php?action=add">
+        <label>
+            Title
+            <input type="text" name="title" required>
+        </label>
+        <label>
+            Text
+            <textarea name="text" rows="8" required></textarea>
+        </label>
+        <input type="submit" value="Add article">
+    </form>
+</div>
