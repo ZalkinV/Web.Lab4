a3f0ae98ee1388d9ac1ce17a2671b5571c6b47ef
Zalkin <Zalkin.viktor@mail.ru>
2019-05-02 20:06:06 +0300

    A.I. Copy index.html here

--- admin/index.php
+++ admin/index.php
@@ -1,1 +1,21 @@
 <?php
+require_once "models/database.php";
+require_once "views/header.html";
+require_once "views/visual_functions.php"
+?>
+
+<div class="container">
+    <?php
+    $articles = get_articles();
+    foreach ($articles as $article): ?>
+    <h1>
+        <a href="/article.php?id=<?=$article["id"]?>">
+            <?= $article["title"] ?>
+        </a>
+    </h1>
+    <p>
+        <?= shorten_text($article["text"]) ?>
+    </p>
+    <hr>
+    <?php endforeach; ?>
+</div>
