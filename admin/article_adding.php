85963e4ac7e662779e04ce0d978a674af6597747
Zalkin <Zalkin.viktor@mail.ru>
2019-05-02 21:42:33 +0300

    A.AA. Add bootstrap styles for form

--- admin/article_adding.php
+++ admin/article_adding.php
@@ -3,16 +3,18 @@ require_once "../views/header.html";
 ?>
 
 <div class="container">
-    <h1>Create new article</h1>
+    <h1 class="text-center">Create new article</h1>
     <form method="post" action="index.php?action=add">
-        <label>
-            Title
-            <input type="text" name="title" required>
-        </label>
-        <label>
-            Text
-            <textarea name="text" rows="8" required></textarea>
-        </label>
-        <input type="submit" value="Add article">
+        <div class="form-group">
+            <label class="h2">Title</label>
+            <input class="form-control" type="text" name="title" required>
+        </div>
+        <div class="form-group">
+            <label class="h2">Text</label>
+            <textarea class="form-control" name="text" rows="8" required></textarea>
+        </div>
+        <div class="form-group">
+            <input class="btn btn-success" type="submit" value="Create">
+        </div>
     </form>
 </div>
