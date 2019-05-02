4575581b0fbf94cdc7e323ad1d6b531e8af3f862
Zalkin <Zalkin.viktor@mail.ru>
2019-05-02 19:24:29 +0300

    A. Add showing article

--- article.php
+++ article.php
@@ -5,4 +5,18 @@ $article_id = $_GET["id"];
 if (!is_numeric($article_id))
     exit();
 
+require_once "views/header.html";
+require_once "models/database.php";
+
+$article = get_article($article_id)
+
 ?>
+
+<div class="container">
+    <h1>
+       <?=$article["title"]?>
+    </h1>
+    <p>
+        <?=$article["text"]?>
+    </p>
+</div>
